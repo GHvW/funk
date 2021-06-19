70ec6e80faf172db5f4519f9c5ef041e53d45611
Garrett van Wageningen <ghvanw283@gmail.com>
2021-06-18 22:31:03 -0500

    Create ActionExtensions.cs

---
+++ src/Funk/ActionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funk {
+
+    public static class ActionExtensions {
+
+        public static Action<(A, B)> Tupled<A, B>(this Action<A, B> action) =>
+            (data) => action(data.Item1, data.Item2);
+
+
+        public static Func<A, Action<B>> Curry<A, B>(this Action<A, B> action) =>
+            (a) => (b) => action(a, b);
+    }
+}
