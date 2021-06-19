5f58dd288391053a3adb0a2c84ac78c81df23f35
Garrett van Wageningen <ghvanw283@gmail.com>
2021-06-18 21:57:35 -0500

    porting over code from Heresy

--- src/Funk/FuncExtensions.cs
+++ src/Funk/FuncExtensions.cs
@@ -5,6 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Funk {
-    class FuncExtensions {
+
+    public static class FuncExtensions {
+
+        public static Func<A, C> AndThen<A, B, C>(this Func<A, B> f, Func<B, C> g) =>
+            (x) => g(f(x));
+
+
+        public static Func<A, C> Compose<A, B, C>(this Func<B, C> f, Func<A, B> g) =>
+            (x) => f(g(x));
+
+
+        public static Func<(A, B), C> Tupled<A, B, C>(this Func<A, B, C> fn) {
+            return data => {
+                var (a, b) = data;
+                return fn(a, b);
+            };
+        }
+
+
+        public static Func<A, Func<B, C>> Curry<A, B, C>(this Func<A, B, C> fn) =>
+            (a) => (b) => fn(a, b);
+
+
+        public static Func<A, Func<B, Func<C, D>>> Curry<A, B, C, D>(this Func<A, B, C, D> fn) =>
+            (a) => (b) => (c) => fn(a, b, c);
     }
 }
---
+++ test/Funk.Test/FuncExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Funk.Test {
+
+    public class FuncExtensionsTests {
+
+        public static class Adder {
+
+            public static int Add(int a, int b) => a + b;
+        }
+
+
+        [Fact]
+        public void Curry1Test() {
+
+            Func<int, int, int> adder = (a, b) => a + b;
+
+            var curried1 = ((Func<int, int, int>) Adder.Add).Curry()(10);
+            var curried2 = adder.Curry()(10);
+
+            var result1 = curried1(5);
+            var result2 = curried2(5);
+
+            Assert.Equal(15, result1);
+            Assert.Equal(15, result2);
+        }
+
+        [Fact]
+        public void Curry2Test() {
+
+            Func<int, int, int> adder = (a, b) => a + b;
+
+            var curried1 = new Func<int, int, int>(Adder.Add).Curry();
+            var curried2 = adder.Curry();
+
+            Assert.Equal(15, curried1(5)(10));
+            Assert.Equal(15, curried1(10)(5));
+        }
+
+        [Fact]
+        public void Tupled_AndThen_Play() {
+
+            Func<int, int, int> adder = (a, b) => a + b;
+
+            var fn1 =
+                new Func<int, int, int>(Adder.Add)
+                    .Tupled()
+                    .AndThen((x) => x * 10);
+
+            var fn2 = adder.Tupled().AndThen((x) => x * 10);
+
+            Assert.Equal(150, fn1((5, 10)));
+            Assert.Equal(150, fn2((10, 5)));
+        }
+    }
+}
